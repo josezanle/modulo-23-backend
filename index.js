4ae4c820b19867a19bd3f722400c37020fc1b29f
josezanle <63666176+josezanle@users.noreply.github.com>
2020-11-19 22:38:52 -0300

    este repositorio contiene el backend del calendarApp, utiliza express mongo db y node js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+
+// crear servidor d express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+
+// directorio publico
+app.use( express.static('public'))
+
+// lectura y parseo del body
+app.use( express.json());
+
+// rutas
+app.use('/api/auth', require('./routes/auth'))
+
+// escuchar peticiones
+app.listen(process.env.PORT,() => {
+    console.log(`servidor corriendo en puerto ${process.env.PORT }`);
+});
+
